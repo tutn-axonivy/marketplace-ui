2a4a1244417a6b34b6e299968ce874b5f52ce842
tutn <tu.nguyenthanh@axonactive.com>
2024-05-15 12:19:25 +0700

    Update test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const jsdom = require("jsdom");
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -11,8 +9,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-webpack'),
       require('karma-chrome-launcher'),
-      require('karma-edge-launcher'),
-      require('karma-phantomjs-launcher'),
       require("karma-jsdom-launcher"),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
