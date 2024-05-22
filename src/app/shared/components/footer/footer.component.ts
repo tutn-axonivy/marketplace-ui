a782cacda9b1fa52bc31e1112dbe056f03f08495
tutn <tu.nguyenthanh@axonactive.com>
2024-05-22 16:05:00 +0700

    update theme logic

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  isDarkMode!: boolean;
 
+  subscriptions: Subscription[] = [];
+
+  themeService = inject(ThemeService);
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule, DOCUMENT } from '@angular/common';
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, inject } from '@angular/core';
 import { Theme } from '../../enums/theme.enum';
 import { NavItem } from '../../models/nav-item.model';
+import { ThemeService } from '../../../core/services/theme.service';
+import { Subscription } from 'rxjs';
 
 const DATA_THEME = 'data-bs-theme';
 
@@ -44,38 +46,7 @@ export class HeaderComponent {
     },
   ];
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
-    const localStorage = this.document.defaultView?.localStorage;
-
-    if (localStorage) {
-      this.loadDefaultTheme(localStorage);
-    }
-  }
-
-  loadDefaultTheme(localStorage: Storage) {
-    const theme = localStorage.getItem(DATA_THEME) as Theme;
-    if (theme) {
-      this.setTheme(theme);
-    } else {
-      this.setTheme(Theme.LIGHT);
-    }
-  }
-
-  toggleThemeMode() {
-    this.isDarkMode = !this.isDarkMode;
-    const theme = this.isDarkMode ? Theme.DARK : Theme.LIGHT;
-    this.setTheme(theme);
-    localStorage.setItem(DATA_THEME, theme);
-  }
-
-  setTheme(theme: Theme) {
-    document.querySelector('html')!.setAttribute(DATA_THEME, theme);
-    if (theme === Theme.DARK) {
-      this.isDarkMode = true;
-    } else {
-      this.isDarkMode = false;
-    }
-  }
+  themeService = inject(ThemeService);
 
   onNavClick(navLink: string) {
     this.selectedNav = navLink;
