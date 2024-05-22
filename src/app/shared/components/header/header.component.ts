0d850c536f26636345011b4fb202ba82522c635f
tutn <tu.nguyenthanh@axonactive.com>
2024-05-22 14:46:30 +0700

    Update header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,48 @@
-import { DOCUMENT } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { Component, Inject, Input } from '@angular/core';
 import { Theme } from '../../enums/theme.enum';
+import { NavItem } from '../../models/nav-item.model';
 
 const DATA_THEME = 'data-bs-theme';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Input() isDarkMode: boolean = false;
+
+  selectedNav: string = '/';
+
+  navItems: NavItem[] = [
+    {
+      label: 'News',
+      link: 'https://developer.axonivy.com/news',
+    },
+    {
+      label: 'Doc',
+      link: 'https://developer.axonivy.com/doc',
+    },
+    {
+      label: 'Tutorial',
+      link: 'https://developer.axonivy.com/tutorial',
+    },
+    {
+      label: 'Community',
+      link: 'https://community.axonivy.com/',
+    },
+    {
+      label: 'Team',
+      link: 'https://developer.axonivy.com/team',
+    },
+    {
+      label: 'Market',
+      link: '/',
+    },
+  ];
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     const localStorage = this.document.defaultView?.localStorage;
@@ -45,5 +75,9 @@ export class HeaderComponent {
     } else {
       this.isDarkMode = false;
     }
+  }
+
+  onNavClick(navLink: string) {
+    this.selectedNav = navLink;
   }
 }
